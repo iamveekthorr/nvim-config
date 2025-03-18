8bf8dd3c415c0ea9fc28287e02225c30004dc6fe
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-03-18 09:56:28 +0100

    feat(plugins): Add mini.surround

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,18 @@
+return {
+  "echasnovski/mini.surround",
+  version = "*",
+  event = "BufEnter",
+  opts = {
+    mappings = {
+      add = "gsa",
+      delete = "gsd",
+      find = "",
+      find_left = "",
+      highlight = "",
+      replace = "gsr",
+      update_n_lines = "",
+      suffix_last = "",
+      suffix_next = "",
+    },
+  },
+}
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,6 +3,12 @@ return {
   opts = {
     spec = {
       { "<leader>w", hidden = true },
+      { "gs", desc = "Surround" },
+      { "gsa", desc = "Surround with ..." },
+      { "gsd", desc = "Delete surrounding ..." },
+      { "gsr", desc = "Replace surrounding ... with ..." },
+
+      { "<leader>o", desc = "Other" },
     },
   },
 }

6486972f38f99584e1cbe47e85e4a18491da5061
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2025-03-29 00:39:52 +0100

    refactor(blink): Update config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,12 +3,19 @@ return {
   event = "InsertEnter",
   opts = {
     keymap = {
-      ["<Tab>"] = { "show" },
+      ["<Tab>"] = { "show_and_insert", "select_next" },
+      ["<C-c>"] = { "cancel" },
+      ["<S-Tab>"] = { "select_prev" },
     },
     completion = {
       trigger = {
         show_on_keyword = false,
         show_on_trigger_character = false,
+        show_on_insert_on_trigger_character = false,
+        show_on_accept_on_trigger_character = false,
+      },
+      ghost_text = {
+        enabled = false,
       },
     },
   },

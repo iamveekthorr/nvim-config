0c676c15b7fe239b4ffb2c1b73a4d2caaf1d0977
iamveekthorr <victorokonkwo67@gmail.com>
2025-07-31 17:22:08 +0100

    fix: turn off which key with added configs
    - which key was not allowing me enter visual mode

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,8 +1,8 @@
 return {
   "folke/which-key.nvim",
   enabled = false,
-  opts = {
-    spec = {
+  opts = function(_, opts)
+    opts.spec = {
       { "<leader>w", hidden = true },
       { "gs", desc = "Surround" },
       { "gsa", desc = "Surround with ..." },
@@ -10,6 +10,7 @@ return {
       { "gsr", desc = "Replace surrounding ... with ..." },
 
       { "<leader>o", desc = "Other" },
-    },
-  },
+    }
+    table.insert(opts.spec, { "<leader>v", "v", desc = "Enter visual mode" })
+  end,
 }

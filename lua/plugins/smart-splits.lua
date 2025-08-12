df593aefe1336ed7496cfa7d7892b9222883b39c
iamveekthorr <victorokonkwo67@gmail.com>
2025-08-12 19:23:28 +0100

    fix: update my plugins
    - make sure smart splits doesn't work on dashboard
    - show dashboard when it's just one buffer open in a split window

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -11,6 +11,67 @@ return {
       at_edge = "wrap",
     })
 
+    -- Close all splits when no file buffers are open
+    vim.api.nvim_create_augroup("smart_splits_auto_close", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufDelete", "BufWipeout" }, {
+      group = "smart_splits_auto_close",
+      callback = function(args)
+        -- Don't trigger if it's a special buffer being deleted
+        local deleted_buftype = vim.bo[args.buf].buftype
+        local deleted_filetype = vim.bo[args.buf].filetype
+
+        -- Only process deletion of normal file buffers
+        if deleted_buftype ~= "" or deleted_filetype == "alpha" or deleted_filetype == "snacks_explorer" then
+          return
+        end
+
+        vim.schedule(function()
+          local bufs = vim.api.nvim_list_bufs()
+          local file_buffer_count = 0
+
+          for _, buf in ipairs(bufs) do
+            if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
+              local bufname = vim.api.nvim_buf_get_name(buf)
+              local buftype = vim.bo[buf].buftype
+              local filetype = vim.bo[buf].filetype
+              local is_modified = vim.bo[buf].modified
+              local line_count = vim.api.nvim_buf_line_count(buf)
+
+              -- Count real file buffers (same logic as alpha.lua)
+              if buftype == "" and filetype ~= "alpha" and filetype ~= "snacks_explorer" then
+                if bufname ~= "" then
+                  file_buffer_count = file_buffer_count + 1
+                elseif is_modified then
+                  file_buffer_count = file_buffer_count + 1
+                elseif
+                  line_count > 1 or (line_count == 1 and vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] ~= "")
+                then
+                  file_buffer_count = file_buffer_count + 1
+                end
+              end
+            end
+          end
+
+          -- Only close splits if there are no file buffers and we have multiple windows
+          if file_buffer_count == 0 and vim.fn.winnr("$") > 1 then
+            vim.cmd("only")
+          end
+        end)
+      end,
+    })
+
+    -- Prevent splitting on alpha dashboard
+    vim.api.nvim_create_autocmd("FileType", {
+      group = "smart_splits_auto_close",
+      pattern = "alpha",
+      callback = function()
+        vim.keymap.set("n", "<C-w>s", "<Nop>", { buffer = true })
+        vim.keymap.set("n", "<C-w>v", "<Nop>", { buffer = true })
+        vim.keymap.set("n", "\\", "<Nop>", { buffer = true })
+        vim.keymap.set("n", "|", "<Nop>", { buffer = true })
+      end,
+    })
+
     -- these keymaps will also accept a range,
     -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
     vim.keymap.set("n", "<localleader>h", smartsplits.resize_left, { desc = "Resize left" })
